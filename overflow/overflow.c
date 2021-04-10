3a2bb70b418cdaf32007127802dbdceab915e721
bernard (pop) tat1 <bernard.tatin@ik.me>
2021-04-10 23:54:35 +0200

    overflow: runs with gcc, clang, Open Watcom with or without GNU builtins (gcc, clang only)

--- overflow/overflow.c
+++ overflow/overflow.c
@@ -4,24 +4,56 @@
 
 #include <stdio.h>
 #include <stdbool.h>
+#include <limits.h>
+
+#if defined(__WATCOMC__)
+#define __UINT64_MAX__ (unsigned long long)-1
+#define LONG    long long
+#define UL_FORMAT   "%20I64u"
+#define ULMAX ULONGLONG_MAX
+#if defined(with_builtins)
+#undef with_builtins
+#endif
+#else
+#define LONG    long
+#define UL_FORMAT   "%20lu"
+#define ULMAX ULONG_MAX
+#endif
+#define ULONG   unsigned LONG
+
+#if defined(with_builtins)
+static inline bool safe_mul_lu(ULONG m1, ULONG m2, ULONG *result) {
+    return !__builtin_umull_overflow(m1, m2, result);
+}
+#else
+static inline bool safe_mul_lu(ULONG m1, ULONG m2, ULONG *result) {
+    if (m1 > ULMAX / m2) {
+        *result = ULMAX;
+        return false;
+    } else {
+        *result = m1 * m2;
+        return true;
+    }
+}
+#endif
 
 int main(void) {
-    unsigned long N = 1, M = 1;
-    unsigned long result = 1;
+    ULONG N = 1, M = 1;
+    ULONG result = 1;
     bool end = false;
 
     printf("\n");
     do {
-        if (__builtin_umull_overflow(N, M, &result)) {
-            printf("KO %10lu\r", N);
+        if (!safe_mul_lu(N, M, &result)) {
+            printf("KO " UL_FORMAT " " UL_FORMAT "\n", N, result);
             end = true;
         } else {
-            printf("OK %10lu %10lu\r", N, M);
+            printf("OK " UL_FORMAT " " UL_FORMAT "\n", N, result);
             M = result;
             N++;
         }
     } while (!end);
-    printf("Last N: %lu\n", N - 1);
+    printf("\nLast N: " UL_FORMAT "\n", N - 1);
 
     return 0;
 }
