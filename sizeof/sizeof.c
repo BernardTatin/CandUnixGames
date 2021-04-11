a3ed8d5fed51927391f6879f72916f1b063dc39c
bernard (pop) tat1 <bernard.tatin@ik.me>
2021-04-12 00:40:07 +0200

    overflow, sizeof: run with nvc, the NVidia compiler - but without NVidia GPU

--- sizeof/sizeof.c
+++ sizeof/sizeof.c
@@ -14,10 +14,12 @@
 
 static char *get_compiler_name(void) {
     static char compiler_name[256];
-#if defined(__GNUC__) && !defined(__clang__)
-    sprintf(compiler_name, "gcc %d.%d.%d", __GNUC__, __GNUC_MINOR__, __GNUC_PATCHLEVEL__);
-#elif defined(__clang__)
+#if defined(__clang__)
     sprintf(compiler_name, "clang %s", __clang_version__);
+#elif defined(__NVCOMPILER)
+    sprintf(compiler_name, "NVidia Compiler %d.%d.%d", __NVCOMPILER_MAJOR__, __NVCOMPILER_MINOR__, __NVCOMPILER_PATCHLEVEL__);
+#elif defined(__GNUC__)
+    sprintf(compiler_name, "gcc %d.%d.%d", __GNUC__, __GNUC_MINOR__, __GNUC_PATCHLEVEL__);
 #elif defined(__WATCOMC__)
     sprintf(compiler_name, "Open Watcom %2d.%02d", __WATCOMC__/100, __WATCOMC__%100);
 #else
