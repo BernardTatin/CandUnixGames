042b13204c6d6b1cbfd59566bdfe27d97c2d2e53
bernard (pop) tat1 <bernard.tatin@ik.me>
2021-04-15 18:40:42 +0200

    overflow.c: using include/safe-int-arithmetic.h

--- overflow/overflow.c
+++ overflow/overflow.c
@@ -7,32 +7,11 @@
  */
 
 #include <stdio.h>
+#include <string.h>
 #include <stdbool.h>
-#include <stdint.h>
-#include <limits.h>
 
+#include "safe-int-arithmetic.h"
 #include "compiler-version.h"
-
-// if no builtins or the wanted builtin
-// is not accessible, we undef with_builtins
-# if defined(with_builtins)
-#if defined(__has_builtin)
-#    if !(__has_builtin(__builtin_umull_overflow))
-#       undef with_builtins
-#    else
-#       warning "With builtins"
-#    endif
-#else
-//   __has_builtin is not defined in Watcom C and
-//   is not defined in gcc 9 but is defined in gcc 10 and clang 9 and 10
-#    if defined(__WATCOMC__) \
-            || (defined(__GNUC__) \
-            && !defined(__clang__) \
-            && (__GNUC__ > 9))
-#       undef with_builtins
-#    endif
-#endif
-#endif
 
 #if defined(with_builtins)
 static char *has_builtin_mul = "with builtins";
@@ -41,39 +20,6 @@ static char *has_builtin_mul = "NO builtins";
 #endif
 
 
-#if defined(__WATCOMC__)
-//  the only integer type with 64 bits
-#   define LONG                long long
-//  printing format for unsigned long long
-#   define UL_FORMAT           "%20I64u"
-//  max unsigned long long value
-#   define ULMAX               ULONGLONG_MAX
-#else
-//  first type with 64 bits
-#   define LONG                long
-//  printing format for unsigned long
-#   define UL_FORMAT           "%20lu"
-//  max unsigned long value
-#   define ULMAX               ULONG_MAX
-#endif
-// because we test factorial which is only defined
-// for positive integers, we use unsigned values
-#define ULONG               unsigned LONG
-
-// *result = m1 * m2
-// if there is overflow, return false, else return true
-static inline bool safe_mul_lu(ULONG m1, ULONG m2, ULONG *result) {
-#if defined(with_builtins)
-    return !__builtin_umull_overflow(m1, m2, result);
-#else
-    *result = m1 * m2;
-    if (m1 > ULMAX / m2) {
-        return false;
-    } else {
-        return true;
-    }
-#endif
-}
 
 int main(void) {
     ULONG N = 1, M = 1;
