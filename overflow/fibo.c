026f9c000e31b73a62d8456d1bde8366825b02f2
bernard (pop) tat1 <bernard.tatin@ik.me>
2021-09-11 04:48:20 +0200

    fibo.c: testing formating options, the '=' key in Vim mode

--- overflow/fibo.c
+++ overflow/fibo.c
@@ -2,12 +2,12 @@
  * fibo.c
  */
 
+#include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
-#include <stdbool.h>
 
-#include "safe-int-arithmetic.h"
 #include "compiler-version.h"
+#include "safe-int-arithmetic.h"
 
 #if defined(with_builtins)
 static char *has_builtin_add = "with builtins";
@@ -16,59 +16,57 @@ static char *has_builtin_add = "NO builtins";
 #endif
 
 static bool compute_fibo(ULONG N, ULONG *result) {
-    ULONG N1 = 1, N2 = 1;
+  ULONG N1 = 1, N2 = 1;
 
-    if (N < 3) {
-        int littlen = (int)N;
-        switch (littlen)
-        {
-        case 0:
-            *result = 0;
-            return true;
-        case 1:
-        case 2:
-            *result = 1;
-            return true;
-        }
+  if (N < 3) {
+    int littlen = (int)N;
+    switch (littlen) {
+    case 0:
+      *result = 0;
+      return true;
+    case 1:
+    case 2:
+      *result = 1;
+      return true;
     }
-    *result = 1;
-    for (ULONG i=2; i<N; i++) {
-        if (!safe_add_lu(N1, N2, result)) {
-            return false;
-        }
-        N1 = N2;
-        N2 = *result;
+  }
+  *result = 1;
+  for (ULONG i = 2; i < N; i++) {
+    if (!safe_add_lu(N1, N2, result)) {
+      return false;
     }
-    return true;
+    N1 = N2;
+    N2 = *result;
+  }
+  return true;
 }
 
 static void show_all_fibos(void) {
-    ULONG N = 0;
-    ULONG result;
-    bool end = false;
+  ULONG N = 0;
+  ULONG result;
+  bool end = false;
 
-    do {
-          if (!compute_fibo(N, &result)) {
-            printf("KO " UL_FORMAT " " UL_FORMAT "\n", N, result);
-            end = true;
-        } else {
+  do {
+    if (!compute_fibo(N, &result)) {
+      printf("KO " UL_FORMAT " " UL_FORMAT "\n", N, result);
+      end = true;
+    } else {
 #if !defined(test_timing)
-            printf("OK " UL_FORMAT " " UL_FORMAT "\n", N, result);
-#endif            
-            N++;
-        }
-    } while (!end);
+      printf("OK " UL_FORMAT " " UL_FORMAT "\n", N, result);
+#endif
+      N++;
+    }
+  } while (!end);
 }
 
-
 int main(void) {
-    printf("%s\n", get_compiler_name(has_builtin_add));
+  printf("%s\n", get_compiler_name(has_builtin_add));
 #if defined(test_timing)
-    for (int i=0; i<500000; i++) {
-        show_all_fibos();
-    }
-#else
+  for (int i = 0; i < 500000; i++) {
     show_all_fibos();
-#endif    
-    return 0;
+  }
+#else
+  show_all_fibos();
+#endif
+  return 0;
 }
